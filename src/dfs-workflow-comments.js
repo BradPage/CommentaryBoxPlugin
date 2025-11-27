6214fa35f47bea7581b11128ce8fd95477467b15
BradPage <bradpage_@hotmail.com>
2025-11-27 16:43:08 +0000

    removed all traces of newcomment

--- src/dfs-workflow-comments.js
+++ src/dfs-workflow-comments.js
@@ -156,7 +156,6 @@ class CommentsElement extends LitElement {
     const outputobj = {
       comments: this.workingComments,
       mostRecentComment: null,
-      hasNewComment: false
     };
     this.dispatchEvent(new CustomEvent('ntx-value-change', { detail: outputobj }));
   }
@@ -240,7 +239,6 @@ class CommentsElement extends LitElement {
     const outputobj = {
       comments: this.workingComments,
       mostRecentComment,
-      hasNewComment: this.deletableIndices.length > 0
     };
 
     this.dispatchEvent(new CustomEvent('ntx-value-change', { detail: outputobj }));
